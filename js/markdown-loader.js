5d830a7b22a1ea68219e339dd423972881736f35
SchlauHaus <jo-rudolph@schlau.haus>
2024-12-20 15:24:37 +0100

    Neuer Ansatz, das Backend soll dateinamen (Markdown) als parameter in einem Headereintrag (SSL-Verschlüsselt wegen https) übergeben. wird durch fetch (markdownloader.js) abgefragt. Dateiname sollte in url stehen. mir fällt gerade auf, dass ich js den dateinamen direkt aus der URL lesen kann lol.

--- js/markdown-loader.js
+++ js/markdown-loader.js
@@ -7,7 +7,7 @@ renderer.link = function (token) {
     const basePath = '../Dokumentation/';
 
     let href = basePath + token.href; // Präfix einfügen
-    console.log(token);
+    //console.log(token);
 
     // Optional: Link-Target auf '_blank' setzen
     // const target = href.startsWith('http') ? ' target="_blank"' : '';
@@ -30,11 +30,37 @@ async function loadMarkdown(filePath) {
     }
 }
 
+async function fetchhead(){
+	const url = window.location.href;
+	// const get_file_url = window.location.host + /file_name
+	console.log("url: ",url);
+	var param;
+	try{
+		response = await fetch(url);
+		if (!response.ok){
+			throw new Error(`Response status: ${response.status}`);
+		}
+		console.log(response);
+		const param = response.headers.get('X-File-Param');
+		console.log("Paralympics");
+		console.log("Parameter: ", param);
+	}	catch(error){
+		console.log("ERR:");
+		console.log(error);
+	}
+// Beispiel: Lade eine bestimmte Markdown-Datei
+	if(param){
+		loadMarkdown(param);
+	}else{
+		loadMarkdown("Hochzeit.md");
+	}
+}
+
 // Marked.js mit dem benutzerdefinierten Renderer konfigurieren
 marked.setOptions({ renderer });
 
-// Beispiel: Lade eine bestimmte Markdown-Datei
-loadMarkdown('Hochzeit.md');
+// HTTP-Header auslesen (da sollte Dateiname drin stehen)
+fetchhead();
 
 
 // document.getElementById('markdown-content').innerHTML = marked.parse('# Marked in browser\n\nRendered by **marked**.');
